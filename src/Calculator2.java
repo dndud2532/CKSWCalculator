ecc4a91b2aa3920b9c30c8bfe604de68a5bb2ee5
user <user@user157>
2015-09-22 19:38:14 +0900

    asd

--- src/Calculator2.java
+++ src/Calculator2.java
@@ -4,14 +4,22 @@ public class Calculator2 {
 
 	public static void main(String[] args) {
 		Calculator2 calculator = new Calculator2();
-		
 		int result = calculator.add(1, 2);
 		System.out.println(result);
-		int result1 = calculator.subtract(3, 2);
-		System.out.println(result1);
-		int result2 = calculator.multiply(2, 1);
-		System.out.println(result2);
+		
+		result = calculator.subtract(3, 2);
+		System.out.println(result);
+		
+		result = calculator.multiply(2, 1);
+		System.out.println(result);
+		
+		result = calculator.divide(4, 2);
+		System.out.println(result);
 	
+	}
+	private int divide(int i, int j) {
+		// TODO Auto-generated method stub
+		return i /j;
 	}
 	private int multiply(int i, int j) {
 		// TODO Auto-generated method stub
