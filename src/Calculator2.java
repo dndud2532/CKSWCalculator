ed486a73540f92c6eb9b2f38e885ab930bba3bc0
user <user@user157>
2015-09-22 19:35:16 +0900

    곱셈 완성

--- src/Calculator2.java
+++ src/Calculator2.java
@@ -9,7 +9,13 @@ public class Calculator2 {
 		System.out.println(result);
 		int result1 = calculator.subtract(3, 2);
 		System.out.println(result1);
+		int result2 = calculator.multiply(2, 1);
+		System.out.println(result2);
 	
+	}
+	private int multiply(int i, int j) {
+		// TODO Auto-generated method stub
+		return i * j;
 	}
 	private int subtract(int i, int j) {
 		return i - j;
